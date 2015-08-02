c9322a75a297ddb23f82187615c94eea11ef281d
mprocha <marcelorocha.unb@gmail.com>
2015-08-02 16:27:03 -0300

    Atualização Sun Aug  2 16:26:52 BRT 2015: Alterações no script gridmagnitudes.sh

--- gridmagnitudes.sh
+++ gridmagnitudes.sh
@@ -15,11 +15,11 @@ scale=1.00/4.6/0.05  # min: 1.63 ; max: 4.57
 #file=grid-mR-st6
 #scale=1.00/4.6/0.05 # min: 1.99 ; max: 4.67
 
-xyz2grd /home/marcelo/gmR/$file.txt -G./$file.grd -R$region -I0.5 -V
+xyz2grd $HOME/gmR/$file.txt -G./$file.grd -R$region -I0.5 -V
 
-makecpt -I -T1.5/5.0/0.5 > /home/marcelo/gmR/$file.cpt
-#makecpt -I -T$scale > /home/marcelo/gmR/$file.cpt
-#grd2cpt /home/marcelo/maps/a-grids/$file.grd > ~/maps/a-grids/$file.cpt
+makecpt -I -T1.5/5.0/0.5 > $HOME/gmR/$file.cpt
+#makecpt -I -T$scale > $HOME/gmR/$file.cpt
+#grd2cpt $HOME/maps/a-grids/$file.grd > $HOME/maps/a-grids/$file.cpt
 
 psbasemap -R$region -JM17.0 -Ba2WSen -P -K > $file-quick.ps
 #pscoast -R$region -W1 -JM17.0 -A1000/1 -Di -G255/255/170 -S116/195/250 -O -K -N1 >> $file-quick.ps
@@ -27,8 +27,8 @@ psbasemap -R$region -JM17.0 -Ba2WSen -P -K > $file-quick.ps
 
 
 pscoast  -R$region -JM19.0 -Gc -O -K >> $file-quick.ps
-grdimage /home/marcelo/gmR/$file.grd -JM17 -E300 -C/home/marcelo/gmR/$file.cpt >> $file-quick.ps
+grdimage $HOME/gmR/$file.grd -JM17 -E300 -C$HOME/gmR/$file.cpt >> $file-quick.ps
 pscoast -Q -O -K >> $file-quick.ps
-psscale -C/home/marcelo/gmR/$file.cpt -D5.7i/2.50i/2.00i/0.3i -Ba0.5f0.5:"Magnitudes Values":  -O -E -K >> $file-quick.ps
-#grdcontour /home/marcelo/gmR/$file.grd -JM19 -C/home/marcelo/gmR/$file.cpt > $file-quick.ps
+psscale -C$HOME/gmR/$file.cpt -D5.7i/2.50i/2.00i/0.3i -Ba0.5f0.5:"Magnitudes Values":  -O -E -K >> $file-quick.ps
+#grdcontour $HOME/gmR/$file.grd -JM19 -C$HOME/gmR/$file.cpt > $file-quick.ps
 gv $file-quick.ps&
